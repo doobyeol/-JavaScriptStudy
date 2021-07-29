cccaffb212867de77f09c4d6ab7ba7615cc985ed
doobyeol <doobyeol@gmail.com>
2021-07-30 00:35:01 +0900

    데이터 타입과 변수

--- Basic/dataTypeAndVariable.js
+++ Basic/dataTypeAndVariable.js
@@ -1,1 +1,131 @@
 // 데이터 타입과 변수
+// 1byte 정수형: -128 ~ 127
+char c;
+
+// 4byte 정수형: -2,124,483,648 ~ 2,124,483,647
+int num;
+
+var integer = 10;        // 정수
+var double = 10.12;      // 실수
+var negative = -20;      // 음의 정수
+var binary = 0b01000001; // 2진수
+var octal = 0o101;       // 8진수
+var hex = 0x41;          // 16진수
+
+console.log(binary); // 65
+console.log(octal);  // 65
+console.log(hex);    // 65
+
+// 표기법만 다를뿐 같은 값이다.
+console.log(binary === octal); // true
+console.log(octal === hex);    // true
+
+console.log(1 === 1.0); // true
+
+var result = 4 / 2;
+console.log(result); // 2
+result = 3 / 2;
+console.log(result); // 1.5
+
+var pInf = 10 / 0;  // 양의 무한대
+console.log(pInf);  // Infinity
+
+var nInf = 10 / -0; // 음의 무한대
+console.log(nInf);  // -Infinity
+
+var nan = 1 * 'string'; // 산술 연산 불가
+console.log(nan);       // NaN
+
+var str = "string"; // 큰 따옴표
+str = 'string';     // 작은 따옴표
+str = `string`;     // 백틱(ES6 템플릿 리터럴)
+
+str = "큰 따옴표로 감싼 문자열 내의 '작은 따옴표'는 문자열이다.";
+str = '작은 따옴표로 감싼 문자열 내의 "큰 따옴표"는 문자열이다.';
+
+var str = 'string';
+// 문자열은 유사배열이다.
+for (var i = 0; i < str.length; i++) {
+    console.log(str[i]);
+}
+
+// 문자열을 변경할 수 없다.
+str[0] = 'S';
+console.log(str); // string
+
+var str = 'string';
+console.log(str); // string
+
+str = 'String';
+console.log(str); // String
+
+str += ' test';
+console.log(str); // String test
+
+str = str.substring(0, 3);
+console.log(str); // Str
+
+str = str.toUpperCase();
+console.log(str); // STR
+
+var foo = true;
+var bar = false;
+
+// typeof 연산자는 타입을 나타내는 문자열을 반환한다.
+console.log(typeof foo); // boolean
+console.log(typeof bar); // boolean
+
+var foo = 'Lee';
+foo = null;  // 참조 정보가 제거됨
+
+var foo = null;
+console.log(typeof foo); // object
+
+var foo = null;
+console.log(typeof foo === null); // false
+console.log(foo === null);        // true
+
+// 심볼 key는 이름의 충돌 위험이 없는 유일한 객체의 프로퍼티 키
+var key = Symbol('key');
+console.log(typeof key); // symbol
+
+var obj = {};
+obj[key] = 'value';
+console.log(obj[key]); // value
+
+var score;  // 변수의 선언
+score = 80; // 값의 할당
+score = 90; // 값의 재할당
+score;      // 변수의 참조
+
+// 변수의 선언과 할당
+var average = (50 + 100) / 2;
+
+var x = 1;
+console.log(x); // 1
+
+// 변수의 중복 선언 (권장하지 않음)
+var x = 100;
+console.log(x); // 100
+
+var foo;
+
+console.log(typeof foo);  // undefined
+
+foo = null;
+console.log(typeof foo);  // object
+
+foo = {};
+console.log(typeof foo);  // object
+
+foo = 3;
+console.log(typeof foo);  // number
+
+foo = 3.14;
+console.log(typeof foo);  // number
+
+foo = 'Hi';
+console.log(typeof foo);  // string
+
+foo = true;
+console.log(typeof foo);  // boolean
