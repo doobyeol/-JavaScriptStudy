00e3781fd3363fa38d51afae567ab837c8f038fb
doobyeol <doobyeol@gmail.com>
2021-08-01 00:52:41 +0900

    제어문

--- Basic/controllFlow.js
+++ Basic/controllFlow.js
@@ -34,3 +34,191 @@ if (조건식) {
     // 조건식1과 조건식2가 모두 거짓이면 이 코드 블록이 실행된다.
 }
 
+var num = 2;
+var kind;
+
+// if 문
+if (num > 0) {
+    kind = '양수'; // 음수를 구별할 수 없다
+}
+console.log(kind); // 양수
+
+// if…else 문
+if (num > 0) {
+    kind = '양수';
+} else {
+    kind = '음수'; // 0은 음수가 아니다
+}
+console.log(kind); // 양수
+
+// if…else if 문
+if (num > 0) {
+    kind = '양수';
+} else if (num < 0) {
+    kind = '음수';
+} else {
+    kind = '영';
+}
+console.log(kind); // 양수
+
+var num = 2;
+var kind;
+
+if (num > 0) kind = '양수';
+else if (num < 0) kind = '음수';
+else kind = '영';
+
+console.log(kind); // 양수
+
+// x가 짝수이면 ‘짝수'를 홀수이면 ‘홀수'를 반환한다.
+var x = 2;
+var result;
+
+if (x % 2) { // 2 % 2는 0이고 0은 false로 취급된다.
+    result = '홀수';
+} else {
+    result = '짝수';
+}
+
+console.log(result); // 짝수
+
+// x가 짝수이면 '짝수'를 홀수이면 '홀수'를 반환한다.
+var x = 2;
+
+// 0은 false로 취급된다.
+var result = x % 2 ? '홀수' : '짝수';
+console.log(result); // 짝수
+
+var num = 2;
+
+// 0은 false로 취급된다.
+var kind = num ? (num > 0 ? '양수' : '음수') : '영';
+console.log(kind); // 양수
+
+// 월을 영어로 변환한다. (11 → 'November')
+var month = 11;
+var monthName;
+
+switch (month) {
+    case 1:
+        monthName = 'January';
+        break;
+    case 2:
+        monthName = 'February';
+        break;
+    case 3:
+        monthName = 'March';
+        break;
+    case 4:
+        monthName = 'April';
+        break;
+    case 5:
+        monthName = 'May';
+        break;
+    case 6:
+        monthName = 'June';
+        break;
+    case 7:
+        monthName = 'July';
+        break;
+    case 8:
+        monthName = 'August';
+        break;
+    case 9:
+        monthName = 'September';
+        break;
+    case 10:
+        monthName = 'October';
+        break;
+    case 11:
+        monthName = 'November';
+        break;
+    case 12:
+        monthName = 'December';
+        break;
+    default:
+        monthName = 'Invalid month';
+}
+
+// 반복문
+for (var i = 0; i < 2; i++) {
+    console.log(i);
+}
+
+for (var i = 1; i <= 6; i++) {
+    for (var j = 1; j <= 6; j++) {
+        if (i + j === 6) console.log(`[${i}, ${j}]`);
+    }
+}
+
+var count = 0;
+
+// count가 3보다 작을 때까지 코드 블록을 계속 반복 실행한다.
+while (count < 3) {
+    console.log(count);
+    count++;
+} // 0 1 2
+
+var count = 0;
+
+// 무한루프
+while (true) {
+    console.log(count);
+    count++;
+    // count가 3이면 코드 블록을 탈출한다.
+    if (count === 3) break;
+} // 0 1 2
+
+var count = 0;
+
+// count가 3보다 작을 때까지 코드 블록을 계속 반복 실행한다.
+do {
+    console.log(count);
+    count++;
+} while (count < 3); // 0 1 2
+
+if (true) {
+    break; // Uncaught SyntaxError: Illegal break statement
+}
+
+
+// foo라는 식별자가 붙은 레이블 블록문
+foo: {
+    console.log(1);
+    break foo; // foo 레이블 블록문을 탈출한다.
+    console.log(2);
+}
+
+console.log('Done!');
+
+// outer라는 식별자가 붙은 레이블 for 문
+outer: for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+        // i + j === 3이면 외부 for 문을 탈출한다.
+        if (i + j === 3) break outer;
+    }
+}
+
+console.log('Done!');
+
+// continue 문을 사용하지 않으면 if 문 내에 코드를 작성해야 한다.
+for (var i = 0; i < string.length; i++) {
+    // 'l'이면 카운트를 증가시킨다.
+    if (string[i] === 'l') {
+        count++;
+        // code
+        // code
+        // code
+    }
+}
+
+// continue 문을 사용면 if 문 밖에 코드를 작성할 수 있다.
+for (var i = 0; i < string.length; i++) {
+    // 'l'이 아니면 카운트를 증가시키지 않는다.
+    if (string[i] !== 'l') continue;
+
+    count++;
+    // code
+    // code
+    // code
+}
