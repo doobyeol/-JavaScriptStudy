fbde30799ebaded114534356669a842f8f9b29c3
doobyeol <doobyeol@gmail.com>
2021-08-02 00:30:16 +0900

    APIs

---
+++ Basic/APIs.js
@@ -0,0 +1,26 @@
+// APIs
+// Application Programming Interfaces
+
+// 어떤 로직이 사용되는지 걱정할 필요없이 제공해주는 함수를 통해 원하는 기능을 구현할 수 있다.
+
+
+// 브라우저 자체에서 제공하는 WEB APIs
+
+// DOM APIS = Document Object Model 웹페이지에 있는 요소들을 생성하거나 삭제, 스타일을 바꾸는등 조작 기능
+// Network APIs 서버와의 통신 기능
+// Graphics APOs
+// Audio / Video APIs
+// Devi0ce APIs
+// File APIs
+// Storage APIs
+
+/*
+    HTTP
+    HTTP는 웹 클라이언트와 서버가 어떻게 통신하는, 통신 규약을 정해놓은 것
+    request <-> response
+
+    HTTPS
+    정보를 주고받는 것들이 잘 감싸져있는 보안처리가 잘 된것.
+    암호키를 이용해서 보안처리가 된 정보 전달
+
+*/
---
+++ Basic/browser.js
@@ -0,0 +1,40 @@
+/* Browser 구조 분석
+
+
+window 창의 전체 오브젝트
+document window 안에서 페이지가 표기되는 부분 HTML 문서에서 작성한 요소들 표기
+navogator 브라우저 자체 정보가 담겨진 오브젝트
+bom (browser Object)
+
+*/
+
+console.log(window);
+console.log(this);
+console.log(innerWidth);
+console.log(window.innerWidth);
+alert('hello');
+prompt('please enter your name');
+innerWidth;
+window.navigator;
+
+// window Object는 글로벌 오브젝트이다.
+
+
+/*
+Document
+
+https://developer.mozilla.org/en-US/docs/Web/API/Document
+
+Window
+
+https://developer.mozilla.org/en-US/docs/Web/API/Window
+
+Viewport
+
+https://developer.mozilla.org/en-US/docs/Glossary/layout_viewport
+
+Navigator
+
+https://developer.mozilla.org/en-US/docs/Web/API/Navigator
+*/
+
