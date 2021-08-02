2cb9f1448afd5ec71ed964bbf7148745bca770b0
doobyeol <doobyeol@gmail.com>
2021-08-02 23:20:45 +0900

    타입캐스팅

--- Basic/typeCoercion.js
+++ Basic/typeCoercion.js
@@ -119,3 +119,135 @@ console.log(isTruthy(true));
 console.log(isTruthy('0'));
 console.log(isTruthy({}));
 console.log(isTruthy([]));
+
+// 명시적 타입 변환 (타입캐스팅)
+
+// 1. String 생성자 함수를 new 연산자 없이 호출하는 방법
+// 숫자 타입 => 문자열 타입
+console.log(String(1));        // "1"
+console.log(String(NaN));      // "NaN"
+console.log(String(Infinity)); // "Infinity"
+// 불리언 타입 => 문자열 타입
+console.log(String(true));     // "true"
+console.log(String(false));    // "false"
+
+// 2. Object.prototype.toString 메소드를 사용하는 방법
+// 숫자 타입 => 문자열 타입
+console.log((1).toString());        // "1"
+console.log((NaN).toString());      // "NaN"
+console.log((Infinity).toString()); // "Infinity"
+// 불리언 타입 => 문자열 타입
+console.log((true).toString());     // "true"
+console.log((false).toString());    // "false"
+
+// 3. 문자열 연결 연산자를 이용하는 방법
+// 숫자 타입 => 문자열 타입
+console.log(1 + '');        // "1"
+console.log(NaN + '');      // "NaN"
+console.log(Infinity + ''); // "Infinity"
+// 불리언 타입 => 문자열 타입
+console.log(true + '');     // "true"
+console.log(false + '');    // "false"
+
+// 1. Number 생성자 함수를 new 연산자 없이 호출하는 방법
+// 문자열 타입 => 숫자 타입
+console.log(Number('0'));     // 0
+console.log(Number('-1'));    // -1
+console.log(Number('10.53')); // 10.53
+// 불리언 타입 => 숫자 타입
+console.log(Number(true));    // 1
+console.log(Number(false));   // 0
+
+// 2. parseInt, parseFloat 함수를 사용하는 방법(문자열만 변환 가능)
+// 문자열 타입 => 숫자 타입
+console.log(parseInt('0'));       // 0
+console.log(parseInt('-1'));      // -1
+console.log(parseFloat('10.53')); // 10.53
+
+// 3. + 단항 연결 연산자를 이용하는 방법
+// 문자열 타입 => 숫자 타입
+console.log(+'0');     // 0
+console.log(+'-1');    // -1
+console.log(+'10.53'); // 10.53
+// 불리언 타입 => 숫자 타입
+console.log(+true);    // 1
+console.log(+false);   // 0
+
+// 4. * 산술 연산자를 이용하는 방법
+// 문자열 타입 => 숫자 타입
+console.log('0' * 1);     // 0
+console.log('-1' * 1);    // -1
+console.log('10.53' * 1); // 10.53
+// 불리언 타입 => 숫자 타입
+console.log(true * 1);    // 1
+console.log(false * 1);   // 0
+
+// 1. Boolean 생성자 함수를 new 연산자 없이 호출하는 방법
+// 문자열 타입 => 불리언 타입
+console.log(Boolean('x'));       // true
+console.log(Boolean(''));        // false
+console.log(Boolean('false'));   // true
+// 숫자 타입 => 불리언 타입
+console.log(Boolean(0));         // false
+console.log(Boolean(1));         // true
+console.log(Boolean(NaN));       // false
+console.log(Boolean(Infinity));  // true
+// null 타입 => 불리언 타입
+console.log(Boolean(null));      // false
+// undefined 타입 => 불리언 타 입
+console.log(Boolean(undefined)); // false
+// 객체 타입 => 불리언 타입
+console.log(Boolean({}));        // true
+console.log(Boolean([]));        // true
+
+// 2. ! 부정 논리 연산자를 두번 사용하는 방법
+// 문자열 타입 => 불리언 타입
+console.log(!!'x');       // true
+console.log(!!'');        // false
+console.log(!!'false');   // true
+// 숫자 타입 => 불리언 타입
+console.log(!!0);         // false
+console.log(!!1);         // true
+console.log(!!NaN);       // false
+console.log(!!Infinity);  // true
+// null 타입 => 불리언 타입
+console.log(!!null);      // false
+// undefined 타입 => 불리언 타입
+console.log(!!undefined); // false
+// 객체 타입 => 불리언 타입
+console.log(!!{});        // true
+console.log(!![]);        // true
+
+// 단축 평가
+
+// 논리합(||) 연산자
+'Cat' || 'Dog'  // 'Cat'
+false || 'Dog'  // 'Dog'
+'Cat' || false  // 'Cat'
+
+// 논리곱(&&) 연산자
+'Cat' && 'Dog'  // Dog
+false && 'Dog'  // false
+'Cat' && false  // false
+
+var elem = null;
+
+console.log(elem.value); // TypeError: Cannot read property 'value' of null
+console.log(elem && elem.value); // null
+
+// 단축 평가를 사용한 매개변수의 기본값 설정
+function getStringLength(str) {
+    str = str || '';
+    return str.length;
+}
+
+getStringLength();     // 0
+getStringLength('hi'); // 2
+
+// ES6의 매개변수의 기본값 설정
+function getStringLength(str = '') {
+    return str.length;
+}
+
+getStringLength();     // 0
+getStringLength('hi'); // 2
