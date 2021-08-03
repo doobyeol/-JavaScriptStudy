abf8de9056b712c9e7cf41c2feb8649286fac79f
doobyeol <doobyeol@gmail.com>
2021-08-04 00:17:49 +0900

    객체 Object

--- Basic/object.js
+++ Basic/object.js
@@ -185,3 +185,210 @@ while with  yield
 *는 ES6에서 추가된 예약어
 */
 
+/* 프로퍼티 값 읽기
+마침표(.) 표기법, 
+대괄호([]) 표기법
+*/
+
+var person = {
+    'first-name': 'Ung-mo',
+    'last-name': 'Lee',
+    gender: 'male',
+    1: 10
+};
+
+console.log(person);
+
+console.log(person.first - name);    // NaN: undefined-undefined
+console.log(person[first - name]);   // ReferenceError: first is not defined
+console.log(person['first-name']); // 'Ung-mo'
+
+console.log(person.gender);    // 'male'
+console.log(person[gender]);   // ReferenceError: gender is not defined
+console.log(person['gender']); // 'male'
+
+console.log(person['1']); // 10
+console.log(person[1]);   // 10 : person[1] -> person['1']
+console.log(person.1);    // SyntaxError
+
+// 프로퍼티 키가 유효한 자바스크립트 이름이고 예약어가 아닌 경우 프로퍼티 값은 마침표 표기법, 대괄호 표기법 모두 사용할 수 있다.
+// 대괄호 내에 들어가는 프로퍼티 이름은 반드시 문자열이어야 한다.
+
+
+/* 프로퍼티 값 갱신
+객체가 소유하고 있는 프로퍼티에 새로운 값을 할당하면 프로퍼티 값은 갱신된다.
+*/
+
+var person = {
+    'first-name': 'Ung-mo',
+    'last-name': 'Lee',
+    gender: 'male',
+};
+
+person['first-name'] = 'Kim';
+console.log(person['first-name']); // 'Kim'
+
+/* 프로퍼티 동적 생성
+객체가 소유하고 있지 않은 프로퍼티 키에 값을 할당하면 하면 주어진 키와 값으로 프로퍼티를 생성하여 객체에 추가
+*/
+
+var person = {
+    'first-name': 'Ung-mo',
+    'last-name': 'Lee',
+    gender: 'male',
+};
+
+person.age = 20;
+console.log(person.age); // 20
+
+/* 프로퍼티 삭제
+delete 연산자를 사용하면 객체의 프로퍼티를 삭제할 수 있다. 이때 피연산자는 프로퍼티 키이어야 한다.
+*/
+
+var person = {
+    'first-name': 'Ung-mo',
+    'last-name': 'Lee',
+    gender: 'male',
+};
+
+delete person.gender;
+console.log(person.gender); // undefined
+
+delete person;
+console.log(person); // Object {first-name: 'Ung-mo', last-name: 'Lee'}
+
+/* for-in 문
+for-in 문을 사용하면 객체(배열 포함)에 포함된 모든 프로퍼티에 대해 루프를 수행할 수 있다.
+*/
+var person = {
+    'first-name': 'Ung-mo',
+    'last-name': 'Lee',
+    gender: 'male'
+};
+
+// prop에 객체의 프로퍼티 이름이 반환된다. 단, 순서는 보장되지 않는다.
+for (var prop in person) {
+    console.log(prop + ': ' + person[prop]);
+}
+
+/*
+first-name: Ung-mo
+last-name: Lee
+gender: male
+*/
+
+var array = ['one', 'two'];
+
+// index에 배열의 경우 인덱스가 반환된다
+for (var index in array) {
+    console.log(index + ': ' + array[index]);
+}
+
+/*
+0: one
+1: two
+*/
+
+/* for-in 문은 객체의 문자열 키(key)를 순회하기 위한 문법이다. 배열에는 사용하지 않는 것이 좋다.
+1. 객체의 경우, 프로퍼티의 순서가 보장되지 않는다. 
+그 이유는 원래 객체의 프로퍼티에는 순서가 없기 때문이다. 
+배열은 순서를 보장하는 데이터 구조이지만 객체와 마찬가지로 순서를 보장하지 않는다.
+2. 배열 요소들만을 순회하지 않는다.
+*/
+
+var array = ['one', 'two'];
+array.name = 'my array';
+
+for (var index in array) {
+    console.log(index + ': ' + array[index]);
+}
+
+/*
+0: one
+1: two
+name: my array
+*/
+
+// for-in 문의 단점을 극복하기 위해 ES6에서 for-of 문이 추가되었다.
+
+const array = [1, 2, 3];
+array.name = 'my array';
+
+for (const value of array) {
+    console.log(value);
+}
+
+/*
+1
+2
+3
+*/
+
+for (const [index, value] of array.entries()) {
+    console.log(index, value);
+}
+
+/*
+0 1
+1 2
+2 3
+*/
+
+// for–in 문은 객체의 프로퍼티를 순회하기 위해 사용하고 
+// for–of 문은 배열의 요소를 순회하기 위해 사용
+
+/*  Pass-by-reference
+object type을 객체 타입 또는 참조 타입이라 한다.
+참조 타입이란 객체의 모든 연산이 실제값이 아닌 참조값으로 처리됨을 의미
+객체 타입은 동적으로 변화할 수 있으므로 어느 정도의 메모리 공간을 확보해야 하는지
+예측할 수 없기 때문에 런타임에 메모리 공간을 확보하고 메모리의 힙 영역에 저장된다.
+이에 반해 원시 타입은 값(value)으로 전달된다. 
+즉, 복사되어 전달된다. 이를 pass-by-value라 한다.
+*/
+// Pass-by-reference
+var foo = {
+    val: 10
+}
+
+var bar = foo;
+console.log(foo.val, bar.val); // 10 10
+console.log(foo === bar);      // true
+
+bar.val = 20;
+console.log(foo.val, bar.val); // 20 20
+console.log(foo === bar);      // true
+
+var foo = { val: 10 };
+var bar = { val: 10 };
+
+console.log(foo.val, bar.val); // 10 10
+console.log(foo === bar);      // false
+
+var baz = bar;
+
+console.log(baz.val, bar.val); // 10 10
+console.log(baz === bar);      // true
+
+var a = {}, b = {}, c = {}; // a, b, c는 각각 다른 빈 객체를 참조
+console.log(a === b, a === c, b === c); // false false false
+
+a = b = c = {}; // a, b, c는 모두 같은 빈 객체를 참조
+console.log(a === b, a === c, b === c); // true true true
+
+
+/* Pass-by-value
+원시 타입은 값(value)으로 전달된다. 
+즉, 값이 복사되어 전달된다. 
+이를 pass-by-value(값에 의한 전달)라 한다.
+*/
+
+// Pass-by-value
+var a = 1;
+var b = a;
+
+console.log(a, b);    // 1  1
+console.log(a === b); // true
+
+a = 10;
+console.log(a, b);    // 1  10
+console.log(a === b); // false
