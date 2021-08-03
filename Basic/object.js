e0022c6dcaabbbce224b56d9c80eac6280fa7194
doobyeol <doobyeol@gmail.com>
2021-08-03 23:59:26 +0900

    객체 Object

--- Basic/object.js
+++ Basic/object.js
@@ -76,3 +76,72 @@ console.log(typeof person); // object
 console.log(person); // {name: "Lee", gender: "male", sayHello: ƒ}
 
 person.sayHello(); // Hi! My name is Lee
+
+// 객체를 생성하는 방법은 객체 리터럴을 사용하는 것이 더 간편하다. 
+// Object 생성자 함수 방식은 특별한 이유가 없다면 그다지 유용해 보이지 않는다.
+
+/* 생성자 함수
+객체 리터럴 방식과 Object 생성자 함수 방식으로 객체를 생성
+=> 프로퍼티 값만 다른 여러 개의 객체를 생성할 때 불편하다.
+*/
+
+var person1 = {
+    name: 'Lee',
+    gender: 'male',
+    sayHello: function () {
+        console.log('Hi! My name is ' + this.name);
+    }
+};
+
+var person2 = {
+    name: 'Kim',
+    gender: 'female',
+    sayHello: function () {
+        console.log('Hi! My name is ' + this.name);
+    }
+};
+
+// 생성자 함수를 사용하면 마치 객체를 생성하기 위한 템플릿(클래스)처럼 사용하여 
+// 프로퍼티가 동일한 객체 여러 개를 간편하게 생성할 수 있다.
+
+// 생성자 함수
+// 생성자 함수 이름은 일반적으로 대문자로 시작한다. 이것은 생성자 함수임을 인식하도록 도움을 준다.
+function Person(name, gender) {
+    this.name = name; // this는 생성자 함수가 생성할 인스턴스(instance)를 가리킨다.
+    this.gender = gender;
+    this.sayHello = function () {
+        console.log('Hi! My name is ' + this.name);
+    };
+}
+
+// 인스턴스의 생성
+var person1 = new Person('Lee', 'male');
+var person2 = new Person('Kim', 'female');
+
+console.log('person1: ', typeof person1);
+console.log('person2: ', typeof person2);
+console.log('person1: ', person1);
+console.log('person2: ', person2);
+
+person1.sayHello();
+person2.sayHello();
+
+
+// 생성자 함수 내에서 선언된 일반 변수는 private(외부에서 참조 불가능)하다. 
+// 즉, 생성자 함수 내부에서는 자유롭게 접근이 가능하나 외부에서 접근할 수 없다.
+function Person(name, gender) {
+    var married = true;         // private
+    this.name = name;           // public
+    this.gender = gender;       // public
+    this.sayHello = function () { // public
+        console.log('Hi! My name is ' + this.name);
+    };
+}
+
+var person = new Person('Lee', 'male');
+
+console.log(typeof person); // object
+console.log(person); // Person { name: 'Lee', gender: 'male', sayHello: [Function] }
+
+console.log(person.gender);  // 'male'
+console.log(person.married); // undefined
