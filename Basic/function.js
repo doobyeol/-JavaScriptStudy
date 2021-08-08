5d611d6207f6ab13af480c86d14162975751602f
doobyeol <doobyeol@gmail.com>
2021-08-08 17:23:40 +0900

    함수

--- Basic/function.js
+++ Basic/function.js
@@ -61,6 +61,121 @@ console.log(foo(10, 10)); // 100
 console.log(bar(10, 10)); // 100
 
 /* Function 생성자 함수
+함수 선언문과 함수 표현식은 모두 함수 리터럴 방식으로 함수를 정의
+=> 내장 함수 Function 생성자 함수로 함수를 생성하는 것을 단순화시킨 short-hand(축약법)이다.
+*/
+
+new Function(arg1, arg2, ...argN, functionBody)
+
+var square = new Function('number', 'return number * number');
+console.log(square(10)); // 100
+
+/* 함수 호이스팅 Hoisting
+자바스크립트는 ES6의 let, const를 포함하여 모든 선언
+(var, let, const, function, function*, class)을 호이스팅(Hoisting)한다.
+*/
+var res = square(5);
+
+function square(number) {
+    return number * number;
+}
+
+// 변수 호이스팅
+// TypeError: square is not a function
+var res = square(5);
+
+var square = function (number) {
+    return number * number;
+}
+
+/* 일급 객체
+생성, 대입, 연산, 인자 또는 반환값으로서의 전달 등 
+프로그래밍 언어의 기본적 조작을 제한없이 사용할 수 있는 대상을 의미
+
+무명의 리터럴로 표현이 가능하다.
+변수나 자료 구조(객체, 배열 등)에 저장할 수 있다.
+함수의 매개변수에 전달할 수 있다.
+반환값으로 사용할 수 있다.
+*/
+
+// 1. 무명의 리터럴로 표현이 가능하다.
+// 2. 변수나 자료 구조에 저장할 수 있다.
+var increase = function (num) {
+    return ++num;
+};
+
+var decrease = function (num) {
+    return --num;
+};
+
+var predicates = { increase, decrease };
+
+// 3. 함수의 매개변수에 전달할 수 있다.
+// 4. 반환값으로 사용할 수 있다.
+function makeCounter(predicate) {
+    var num = 0;
+
+    return function () {
+        num = predicate(num);
+        return num;
+    };
+}
+
+var increaser = makeCounter(predicates.increase);
+console.log(increaser()); // 1
+console.log(increaser()); // 2
+
+var decreaser = makeCounter(predicates.decrease);
+console.log(decreaser()); // -1
+console.log(decreaser()); // -2
+
+/* 매개변수
+매개변수(parameter, 인자) vs 인수(argument)
+매개변수는 함수 내에서 변수와 동일하게 메모리 공간을 확보하며
+함수에 전달한 인수는 매개변수에 할당된다.
+만약 인수를 전달하지 않으면 매개변수는 undefined로 초기화된다.
+*/
+var foo = function (p1, p2) {
+    console.log(p1, p2);
+};
+
+foo(1); // 1 undefined
+
+
+/* Call-by-value (값에 의한 호출)
+ 함수 호출 시 원시 타입 인수를 함수에 매개변수로 전달할 때 
+ 매개변수에 값을 복사하여 함수로 전달하는 방식
+ 이때 함수 내에서 매개변수를 통해 값이 변경되어도 
+ 전달이 완료된 원시 타입 값은 변경되지 않는다.
+*/
+function foo(primitive) {
+    primitive += 1;
+    return primitive;
+}
+
+var x = 0;
+
+console.log(foo(x)); // 1
+console.log(x);      // 0
+
+
+/* Call-by-reference (참조에 의한 호출)
+
+*/
+
+/*
+
+*/
+
+/*
+
+*/
+
+/*
+
+*/
+
+/*
 
 */
 
