d8a7355b0f7523d57897cc4b53e47f7c50e02078
doobyeol <doobyeol@gmail.com>
2021-08-11 01:05:30 +0900

    재귀 함수

--- Basic/function.js
+++ Basic/function.js
@@ -408,13 +408,69 @@ function () {
 (function () {
     // ...
 })();
-/*
 
+(function () {
+    var foo = 1;
+    console.log(foo);
+}());
+
+var foo = 100;
+console.log(foo);
+
+/* 내부 함수
+함수 내부에 정의된 함수
+아래 예제의 내부함수 child는 자신을 포함하고 있는 부모함수 parent의 변수에 접근할 수 있다.
+하지만 부모함수는 자식함수(내부함수)의 변수에 접근할 수 없다.
 */
 
-/*
+// 또한 내부함수는 부모함수의 외부에서 접근할 수 없다.
+function sayHello(name) {
+    var text = 'Hello ' + name;
+    var logHello = function () { console.log(text); }
+    logHello();
+}
 
+sayHello('lee');  // Hello lee
+logHello('lee');  // logHello is not defined
+
+
+/* 재귀 함수
+자기 자신을 호출하는 함수
 */
+// 피보나치 수열
+// 피보나치 수는 0과 1로 시작하며, 다음 피보나치 수는 바로 앞의 두 피보나치 수의 합이 된다.
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, ...
+function fibonacci(n) {
+    if (n < 2) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(0)); // 0
+console.log(fibonacci(1)); // 1
+console.log(fibonacci(2)); // 1
+console.log(fibonacci(3)); // 2
+console.log(fibonacci(4)); // 3
+console.log(fibonacci(5)); // 5
+console.log(fibonacci(6)); // 8
+
+// 팩토리얼
+// 팩토리얼(계승)은 1부터 자신까지의 모든 양의 정수의 곱이다.
+// n! = 1 * 2 * ... * (n-1) * n
+function factorial(n) {
+    if (n < 2) return 1;
+    return factorial(n - 1) * n;
+}
+
+console.log(factorial(0)); // 1
+console.log(factorial(1)); // 1
+console.log(factorial(2)); // 2
+console.log(factorial(3)); // 6
+console.log(factorial(4)); // 24
+console.log(factorial(5)); // 120
+console.log(factorial(6)); // 720
+
+// 재귀 함수는 자신을 무한히 연쇄 호출하므로 호출을 멈출 수 있는 탈출 조건을 반드시 만들어야 한다. 
+// 탈출 조건이 없는 경우, 함수가 무한 호출되어 stackoverflow 에러가 발생
 
 /*
 
